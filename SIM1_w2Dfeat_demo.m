2fe760f69e07f032d55c77a88079747be5451222
J.Y.Chung <robot0321@naver.com>
2019-06-27 15:58:07 +0900

    SIM2 complete
    <SIM2 specification>
    1. world, body, camera frame - 3-D position, attitude applied
    2. Features with Distance, Attitude, Intrinsic Matrix Constraint
    3. Visualization in 3D dimension and projected camera view
    
    Signed-off-by: J.Y.Chung <robot0321@naver.com>

--- SIM1_w2Dfeat_demo.m
+++ SIM1_w2Dfeat_demo.m
@@ -1,41 +1,60 @@
-clc; clear; close all;
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 2차원에서 각도, 거리 조건에 맞는 feature들 골라서 이동하면서 확인하기
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% <SIM1 specification>
+% In 2-D, Features with angle and distance constraint
+% 
+% Copyright with Jae Young Chung, robot0321 at github 
+% Lisence: MIT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc; clear; close all; 
+rng(10);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Nfeatures = 1000;
+%% environment & robot setting
+Nfeatures = 500; % the number of features
 
-path  = [0:0.5:50, 50*ones(size(0:0.5:50)), 50:-0.5:0, zeros(size(50:-0.5:0));
-         zeros(size(0:0.5:50)), 0:0.5:50, 50*ones(size(50:-0.5:0)),50:-0.5:0];
-heading = pi/180*[zeros(size(0:0.5:50)), 90*ones(size(0:0.5:50)), ...
-            180*ones(size(50:-0.5:0)), 270*ones(size(50:-0.5:0))];
+fwd_speed = 2; % speed along the forward direction
+rot_time = 10; % steps needs to be rotated
 
-robot = [path; heading]; %heading은 x으로 부터 각도
+% path and heading of robot, heading is the angle from world-x axis
+path  = [0:fwd_speed:50, 50*ones(1,rot_time), 50*ones(size(0:fwd_speed:50)), 50*ones(1,rot_time),  50:-fwd_speed:0, 0*ones(1,rot_time), zeros(size(50:-fwd_speed:0));
+         zeros(size(0:fwd_speed:50)), 0*ones(1,rot_time), 0:fwd_speed:50, 50*ones(1,rot_time), 50*ones(size(50:-fwd_speed:0)), 50*ones(1,rot_time), 50:-fwd_speed:0];
+heading = pi/180*[zeros(size(0:fwd_speed:50)), linspace(0,90,rot_time), 90*ones(size(0:fwd_speed:50)), ...
+            linspace(90,180,rot_time), 180*ones(size(50:-fwd_speed:0)),linspace(180,270,rot_time), 270*ones(size(50:-fwd_speed:0))]; % yaw (rad)
+robot = [path; heading]; 
          
-rng(10);% x,y:-50~100 범위에 Nfeatures 갯수 만큼 뿌리기
-feat_position = [rand(2,Nfeatures)*100 - 25];
+% x,y: 'Nfeatures' number of features, in range (-50,100)
+feat_position = rand(2,Nfeatures)*100 - 25;
 
-%%
-theta = 60*pi/180; % 초점에서 이미지 외곽으로 각도
-mindist = 2; % 최소 1미터 이상
-maxdist = 30; % 최대 20미터만
+% robot(camera) setting
+theta = 45*pi/180; % angle between principal axis and the edge of the image plane (arbitarilliy selected)
+mindist = 2; % minimum distance from a feature to camera principal point
+maxdist = 20; % maximum distance "
 
+%% driving robot
 for i=1:length(robot)
-    % 조건에 맞는 feature 고르기
+    % Select constrainted features
     currpos = robot(1:2,i);
-    feat_angle = atan2(feat_position(2,:)-currpos(2), feat_position(1,:)-currpos(1)) - robot(3,i);
+    currhead = robot(3,i);
+    
+    % the angle from the principal axis of camera to a line between a feature and camera
+    feat_angle = atan2(feat_position(2,:)-currpos(2), feat_position(1,:)-currpos(1)) - currhead;
+    % compensation due to the uncontinuity of angle from atan2 (-pi ~ pi)
     feat_angle(feat_angle<-pi) = feat_angle(feat_angle<-pi) + 2*pi;
     feat_angle(feat_angle>pi) = feat_angle(feat_angle>pi) - 2*pi;
+    
+    % distance constraint 
     feat_dist = sqrt(sum((feat_position - currpos).^2, 1));
+    
+    % select features with angle and distance constraint
     feat_inview_idx = (feat_angle<theta & feat_angle>-theta & ...
                     feat_dist>mindist & feat_dist<maxdist);
     feat_inview = feat_position(:,feat_inview_idx);
     
+    % draw features, path, robot, and view-line in 2-D
     figure(1);
-    scatter(feat_position(1,:), feat_position(2,:),'b'); hold on;
-    plot(path(1,:), path(2,:),'r'); 
-    scatter(currpos(1),currpos(2), 100,'g');
-    plot([currpos(1)*ones(1,size(feat_inview,2)); feat_inview(1,:)], ...
-         [currpos(2)*ones(1,size(feat_inview,2)); feat_inview(2,:)], 'k'); hold off;
+    scatter(feat_position(1,:), feat_position(2,:),'b'); hold on; %features
+    plot(path(1,:), path(2,:),'r'); % path
+    scatter(currpos(1),currpos(2), 72,'g','filled'); % robot
+    plot([currpos(1)*ones(1,size(feat_inview,2)); feat_inview(1,:)], ... 
+         [currpos(2)*ones(1,size(feat_inview,2)); feat_inview(2,:)], 'k'); % f in view 
+    hold off;
 end
--- SIM2_w3Dfeat_demo.m
+++ SIM2_w3Dfeat_demo.m
@@ -1,70 +1,127 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% <SIM2 specification>
+% 1. world, body, camera frame - 3-D position, attitude applied
+% 2. Features with Distance, Attitude, Intrinsic Matrix Constraint 
+% 3. Visualization in 3D dimension and projected camera view
+% world frame (normal x,y,z), body frame (forward-x, right-y, down-z),
+% camera frame(forward-z, right-x, down-y) 
+% intrinsic matrix (K), extrinsic matrix (Tcb) applied
+% 
+% Copyright with Jae Young Chung, robot0321 at github 
+% Lisence: MIT 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc; clear; close all;
-%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 3차원에서 각도, 거리 조건에 맞는 feature들 골라서 이동하면서 확인하기
-% world frame (x,y,z), body frame (전진방향x, 오른쪽y, 아래z), 
-% camera frame(전진방향z, 오른쪽x, 아래y)
-% intrinsic, extrinsic matrix 적용
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rng(10);
 
-Nfeatures = 1000;
+%% environment & robot setting
+Nfeatures = 500; % the number of features
 
-% world에서 본 body의 위치, 자세 정의
-tbw  = [0:0.5:50, 50*ones(size(0:0.5:50)), 50:-0.5:0, zeros(size(50:-0.5:0));
-         zeros(size(0:0.5:50)), 0:0.5:50, 50*ones(size(50:-0.5:0)),50:-0.5:0;
-         zeros(1, size(0:0.5:50,2)+size(0:0.5:50,2)+size(50:-0.5:0,2)+size(50:-0.5:0,2))];
-tbw = reshape(tbw, 3,1,404);
-heading = pi/180*[zeros(size(0:0.5:50)), 90*ones(size(0:0.5:50)), ...
-            180*ones(size(50:-0.5:0)), 270*ones(size(50:-0.5:0))]; % yaw (rad)
+fwd_speed = 2; % speed along the forward direction
+rot_time = 10; % steps needs to be rotated
+
+% body position and attitude in world view
+tw_bw  = [0:fwd_speed:50, 50*ones(1,rot_time), 50*ones(size(0:fwd_speed:50)), 50*ones(1,rot_time),  50:-fwd_speed:0, 0*ones(1,rot_time), zeros(size(50:-fwd_speed:0));
+         zeros(size(0:fwd_speed:50)), 0*ones(1,rot_time), 0:fwd_speed:50, 50*ones(1,rot_time), 50*ones(size(50:-fwd_speed:0)), 50*ones(1,rot_time), 50:-fwd_speed:0;
+         zeros(1, size(0:fwd_speed:50,2)+size(0:fwd_speed:50,2)+size(50:-fwd_speed:0,2)+size(50:-fwd_speed:0,2)+ rot_time*3)];
+heading = pi/180*[zeros(size(0:fwd_speed:50)), linspace(0,90,rot_time), 90*ones(size(0:fwd_speed:50)), ...
+            linspace(90,180,rot_time), 180*ones(size(50:-fwd_speed:0)),linspace(180,270,rot_time), 270*ones(size(50:-fwd_speed:0))]; % yaw (rad)
 pitch = zeros(size(heading));
 roll = pi*ones(size(heading));
-Rbw = angle2dcm(heading, pitch, roll);
+Rbw = angle2dcm(heading, pitch, roll); % dcm rotation matrix
 
-Tbw = [Rbw, tbw; zeros(1,3,404),ones(1,1,404)]; % T^b_w
+% Transformation matrix world to body T^w_b
+tb_bw = zeros(3,1,size(tw_bw,2));
+for i=1:size(tw_bw,2)
+    tb_bw(:,1,i) = -Rbw(:,:,i)*tw_bw(:,i); 
+end
+Tbw = [Rbw, tb_bw; zeros(1,3,size(tw_bw,2)),ones(1,1,size(tw_bw,2))]; % T^b_w
 
-rng(10);% x,y:-50~100 범위에 Nfeatures 갯수 만큼 뿌리기
-feat_position = [rand(2,Nfeatures)*100 - 25; rand(1,Nfeatures)*10-5];
+% x,y: 'Nfeatures' number of features, in range (-50,100)
+feat_position = [rand(2,Nfeatures)*100 - 25; rand(1,Nfeatures)*30-15];
 
-%%
-Tcb = [angle2dcm(pi/2, 0,pi/2), -[0;0;1e-3]; zeros(1,3), 1]; % T^c_b
-% body에서 전진방향(x)로 1cm만큼에 카메라가 존재한다.
-f = 2e-3; % focal_length
-cx = 320.5; cy=240.5; % 640 x 480
+% robot(camera setting)
+Tcb = [angle2dcm(pi/2, 0,pi/2), -[0;0;1e-3]; zeros(1,3), 1]; % T^c_b, extrinsic
+% -> the origin of camera frame is at the (x = 1e-3m, y = 0, z = 0) in body frame
+f = 2e2; % focal_length
+cx = 320.5; cy=240.5; % 640 x 480 (arbitariliy selected)
 K = [f, 0, cx;
      0, f, cy;
-     0, 0, 1]; % intrinsic
+     0, 0, 1]; % intrinsic matrix
 
- 
-
-theta_row = 60*pi/180; % 초점에서 이미지 외곽으로 각도
-theta_col = 45*pi/180;
-mindist = 2; % 최소 1미터 이상
-maxdist = 30; % 최대 20미터만
+mindist = 2; % minimum distance from a feature to camera principal point
+maxdist = 30; % maximum distance "
 focal_length = 1;
 
+%% driving robot
+for i=1:size(tw_bw,2)
+    % Select constrainted features
+    currpos = tw_bw(:,i);
+    curratt = Rbw; 
 
-for i=1:length(Twb)
-    % 조건에 맞는 feature 고르기
-    currpos = Twb.pos(:,i);
-    curratt = Twb.att(:,:,i);
-    feat_row_angle = atan2(feat_position(2,:)-currpos(2), feat_position(1,:)-currpos(1)) - Twb(4,i);
-    feat_row_angle(feat_row_angle<-pi) = feat_row_angle(feat_row_angle<-pi) + 2*pi;% 
-    feat_row_angle(feat_row_angle>pi) = feat_row_angle(feat_row_angle>pi) - 2*pi;  % 각 불연속 해결
-    feat_col_angle = atan2(feat_position(3,:)-currpos(3), sqrt(sum((feat_position(1:2,:) - currpos(1:2)).^2))) - Twb(5,i);
-    feat_col_angle(feat_col_angle<-pi) = feat_col_angle(feat_col_angle<-pi) + 2*pi;% 
-    feat_col_angle(feat_col_angle>pi) = feat_col_angle(feat_col_angle>pi) - 2*pi;  % 각 불연속 해결
+    % distance constraint
+    feat_dist = sqrt(sum((feat_position - currpos).^2, 1)); 
+    feat_dist_validx = find(feat_dist>mindist & feat_dist<maxdist); 
     
+    % camera attitude constraint
+    feat_cam = Tcb*Tbw(:,:,i)*[feat_position(1:3,feat_dist_validx); ones(1,size(feat_dist_validx,2))];
+    feat_cam_validx = feat_cam(3,:)>0; 
     
-    feat_dist = sqrt(sum((feat_position - currpos).^2, 1));
-    feat_inview_idx = (feat_row_angle<theta_row & feat_row_angle>-theta_row & ...
-                       feat_col_angle<theta_col & feat_col_angle>-theta_col & ...
-                       feat_dist>mindist & feat_dist<maxdist);
-    feat_inview = feat_position(:,feat_inview_idx);
+    % camera intrinsic matrix constraint
+    feat_pixel = K*(feat_cam(1:3,feat_cam_validx)./feat_cam(3,feat_cam_validx));
+    feat_pixel_validx = feat_pixel(1,:)>0 & feat_pixel(1,:)<640 ...
+                        & feat_pixel(2,:)>0 & feat_pixel(2,:)<480; 
+                    
+    % The features with all the constriant in world frame                
+    feat_world_validx = feat_dist_validx(feat_cam_validx); feat_world_validx = feat_world_validx(feat_pixel_validx);
+
+    % index saving for optical flow
+    k=i-1;
+    if(i==1), feat_prevValidx = feat_world_validx; k=i; end
+    feat_currValidx = feat_world_validx;
+
+    figure(1); 
+    % 3-D features, robot, path and the features in view
+    subplot(1,2,1); 
+    scatter3(feat_position(1,:), feat_position(2,:),feat_position(3,:),'b'); hold on;
+    plot3(tw_bw(1,:), tw_bw(2,:), tw_bw(3,:),'r'); 
+    plot3([currpos(1)*ones(1,size(feat_world_validx,2)); feat_position(1,feat_world_validx)], ...
+         [currpos(2)*ones(1,size(feat_world_validx,2)); feat_position(2,feat_world_validx)], ...
+         [currpos(3)*ones(1,size(feat_world_validx,2)); feat_position(3,feat_world_validx)],'k'); 
+    scatter3(currpos(1),currpos(2), currpos(3), 50,'g','filled');
+    axis equal; hold off;
     
-    figure(1);
-    scatter(feat_position(1,:), feat_position(2,:),'b'); hold on;
-    plot(tbw(1,:), tbw(2,:),'r'); 
-    scatter(currpos(1),currpos(2), 100,'g');
-    plot([currpos(1)*ones(1,size(feat_inview,2)); feat_inview(1,:)], ...
-         [currpos(2)*ones(1,size(feat_inview,2)); feat_inview(2,:)], 'k'); hold off;
+    % Features projected on the camera with the constraints and optical flow
+    subplot(1,2,2); 
+    feat_intrsctValidx = intersect(feat_prevValidx, feat_currValidx); % feature which is tracked 
+    feat_currNewPixel = world2cam(feat_position(:,setdiff(feat_currValidx, feat_prevValidx)), K, Tcb*Tbw(:,:,i)); % features which is newly appeared 
+    feat_currTrckPixel = world2cam(feat_position(:,feat_intrsctValidx), K, Tcb*Tbw(:,:,i)); 
+    feat_prevTrckPixel = world2cam(feat_position(:,feat_intrsctValidx), K, Tcb*Tbw(:,:,k));
+    % caution: not-tracked previous features are not drew (because not interested)
+    
+    % current & previous features on camera image plane
+    scatter(feat_currTrckPixel(1,:), feat_currTrckPixel(2,:),50,'r'); hold on;
+    if ~isempty(feat_currNewPixel), scatter(feat_currNewPixel(1,:), feat_currNewPixel(2,:),50,'r'); hold on; end % avoid the error when it is empty
+    scatter(feat_prevTrckPixel(1,:), feat_prevTrckPixel(2,:),'g'); 
+    
+    % optical flow between tracked features
+    plot([feat_prevTrckPixel(1,:); feat_currTrckPixel(1,:)], [feat_prevTrckPixel(2,:); feat_currTrckPixel(2,:)],'-y');
+    axis equal; hold off;
+    axis([0, 640, 0, 480]);
+    
+    % For next step
+    feat_prevValidx = feat_currValidx;
+end
+
+
+function pixel2 = world2cam(feat_world3, K, Tcw)
+    % transformation from world to camera with intrinsic matrix 
+    if isempty(feat_world3)
+        pixel2 = []; return;
+    end
+    if (size(feat_world3,2)==3)
+        feat_world3 = feat_world3';
+    end
+    feat_cam = Tcw*[feat_world3; ones(1,size(feat_world3,2))];
+    feat_pixel = K*(feat_cam(1:3,:)./feat_cam(3,:));
+    pixel2 = feat_pixel(1:2,:);
 end
